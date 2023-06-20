8e10aa275e40dad9148e8fa0b83a2d5d2dfe68b0
vaughn <jwst0208@gmail.com>
2023-06-20 10:58:07 +0900

    performance for each class

--- detector_performance.py
+++ detector_performance.py
@@ -10,41 +10,42 @@ import detectors.denoising_detector as denosing
 import detectors.pca_detector as pca
 import detectors.OPA2D_detector as opa2d
 
+random.seed(42)
 binary = ResNetforOSP()
 
-def __do_attack_detect(image):
+def __do_attack_detect(idx, image):
     global denosing_detect_success_number, pca_detect_success_number, binary_detect_success_number, opa2d_detect_success_number
-    if denosing.is_attack(image) : denosing_detect_success_number += 1
-    if pca.is_attack(image) : pca_detect_success_number += 1
-    if binary.is_attack(image) : binary_detect_success_number += 1
-    if opa2d.is_attack(image) : opa2d_detect_success_number += 1
+    if denosing.is_attack(image) : denosing_detect_success_number[idx] += 1
+    if pca.is_attack(image) : pca_detect_success_number[idx] += 1
+    if binary.is_attack(image) : binary_detect_success_number[idx] += 1
+    if opa2d.is_attack(image) : opa2d_detect_success_number[idx] += 1
 
-def __do_original_detect(image):
+def __do_original_detect(idx, image):
     global original_denosing_detect_success_number, original_pca_detect_success_number, original_binary_detect_success_number, original_opa2d_detect_success_number
-    if not denosing.is_attack(image) : original_denosing_detect_success_number += 1
-    if not pca.is_attack(image) : original_pca_detect_success_number += 1
-    if not binary.is_attack(image) : original_binary_detect_success_number += 1
-    if not opa2d.is_attack(image) : original_opa2d_detect_success_number += 1
+    if not denosing.is_attack(image) : original_denosing_detect_success_number[idx] += 1
+    if not pca.is_attack(image) : original_pca_detect_success_number[idx] += 1
+    if not binary.is_attack(image) : original_binary_detect_success_number[idx] += 1
+    if not opa2d.is_attack(image) : original_opa2d_detect_success_number[idx] += 1
 
 def __init_variable():
     global denosing_detect_success_number, pca_detect_success_number, binary_detect_success_number, opa2d_detect_success_number
     global original_denosing_detect_success_number, original_pca_detect_success_number, original_binary_detect_success_number, original_opa2d_detect_success_number
-    denosing_detect_success_number = 0
-    pca_detect_success_number = 0
-    binary_detect_success_number = 0
-    opa2d_detect_success_number = 0
-    original_denosing_detect_success_number = 0
-    original_pca_detect_success_number = 0
-    original_binary_detect_success_number = 0
-    original_opa2d_detect_success_number = 0
+    denosing_detect_success_number =  [0 for _ in range(10)]
+    pca_detect_success_number =  [0 for _ in range(10)]
+    binary_detect_success_number =  [0 for _ in range(10)]
+    opa2d_detect_success_number =  [0 for _ in range(10)]
+    original_denosing_detect_success_number =  [0 for _ in range(10)]
+    original_pca_detect_success_number =  [0 for _ in range(10)]
+    original_binary_detect_success_number =  [0 for _ in range(10)]
+    original_opa2d_detect_success_number =  [0 for _ in range(10)]
 
 def __load_image_file_name_list():
-    global attack_image_folder, original_image_folder, attack_file_name_list, original_file_name_list, total_image_number, tmp_attack_file, tmp_original_file
-    attack_file_name_list = []
-    original_file_name_list = []
+    global attack_image_folder, original_image_folder, attack_file_name_list, original_file_name_list, total_image_number, tmp_attack_file, tmp_original_file, data_class
+    attack_file_name_list = [[] for _ in range(10)]
+    original_file_name_list = [[] for _ in range(10)]
     attack_image_folder = "C:/Users/CoIn240/VSCpython/2023OSP/one-pixel-attack-keras/resnet_sample/attack"
     original_image_folder = "C:/Users/CoIn240/VSCpython/2023OSP/one-pixel-attack-keras/resnet_sample/original"
-    data_class = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'ship']
+    data_class = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
     
     tmp_attack_file = [f for f in os.listdir(attack_image_folder) if f.endswith('.png')]
     random.shuffle(attack_file_name_list)
@@ -52,11 +53,11 @@ def __load_image_file_name_list():
     tmp_original_file = [f for f in os.listdir(original_image_folder) if f.endswith('.png')]
     random.shuffle(tmp_original_file)
 
-    for class_name in data_class :
+    for i, class_name in enumerate(data_class) :
         count = 0
         for file in tmp_attack_file :
             if file.split('_')[1] == class_name + '.png' :
-                attack_file_name_list.append(file)
+                attack_file_name_list[i].append(file)
                 count += 1
             if count == 10:
                 break
@@ -64,11 +65,11 @@ def __load_image_file_name_list():
             print("error : test image number is more than attack image number !!")
             exit()
 
-    for class_name in data_class :
+    for i, class_name in enumerate(data_class) :
         count = 0
         for file in tmp_original_file :
             if file.split('_')[1] == class_name + '.png' :
-                original_file_name_list.append(file)
+                original_file_name_list[i].append(file)
                 count += 1
             if count == 10:
                 break
@@ -77,48 +78,52 @@ def __load_image_file_name_list():
             exit()
 
 
-def __load_attack_image(index):
+def __load_attack_image(class_idx, idx):
     global attack_image_folder, attack_file_name_list
-    image_path = os.path.join(attack_image_folder, attack_file_name_list[index])
+    image_path = os.path.join(attack_image_folder, attack_file_name_list[class_idx][idx])
     image = cv2.imread(image_path, cv2.IMREAD_COLOR)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
 
-def __load_original_image(index):
+def __load_original_image(class_idx, idx):
     global original_image_folder, original_file_name_list
-    image_path = os.path.join(original_image_folder, original_file_name_list[index])
+    image_path = os.path.join(original_image_folder, original_file_name_list[class_idx][idx])
     image = cv2.imread(image_path, cv2.IMREAD_COLOR)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
 
 def __print_result():
-    global total_image_number
+    global total_image_number, data_class
     global denosing_detect_success_number, pca_detect_success_number, binary_detect_success_number, opa2d_detect_success_number
     global original_denosing_detect_success_number, original_pca_detect_success_number, original_binary_detect_success_number, original_opa2d_detect_success_number
+    
+    for i, cls in enumerate(data_class) :
+        print("\n==== class name : " + cls+"====")
+        print("==== Attack Accuracy ====")
+        print("Denosing : " + str(denosing_detect_success_number[i]/total_image_number[i]))
+        print("Pca      : " + str(pca_detect_success_number[i]/total_image_number[i]))
+        print("Binary   : " + str(binary_detect_success_number[i]/total_image_number[i]))
+        print("OPA2D    : " + str(opa2d_detect_success_number[i]/total_image_number[i]))
 
-    print("==== Attack Accuracy ====")
-    print("Denosing : " + str(denosing_detect_success_number/total_image_number))
-    print("Pca      : " + str(pca_detect_success_number/total_image_number))
-    print("Binary   : " + str(binary_detect_success_number/total_image_number))
-    print("OPA2D    : " + str(opa2d_detect_success_number/total_image_number))
-
-    print("==== Original Accuracy ====")
-    print("Denosing : " + str(original_denosing_detect_success_number/total_image_number))
-    print("Pca      : " + str(original_pca_detect_success_number/total_image_number))
-    print("Binary   : " + str(original_binary_detect_success_number/total_image_number))
-    print("OPA2D    : " + str(original_opa2d_detect_success_number/total_image_number))
+        print("==== Original Accuracy ====")
+        print("Denosing : " + str(original_denosing_detect_success_number[i]/total_image_number[i]))
+        print("Pca      : " + str(original_pca_detect_success_number[i]/total_image_number[i]))
+        print("Binary   : " + str(original_binary_detect_success_number[i]/total_image_number[i]))
+        print("OPA2D    : " + str(original_opa2d_detect_success_number[i]/total_image_number[i]))
 
 if __name__ == "__main__":
     global total_image_number
-    total_image_number = 100
+    total_image_number = [10] * 10
     __init_variable()
     __load_image_file_name_list()
-   
-    for index in tqdm(range(total_image_number)):
-        image = __load_attack_image(index)
-        __do_attack_detect(image)
     
-    for index in tqdm(range(total_image_number)):
-        image = __load_original_image(index)
-        __do_original_detect(image)
+    for class_idx in tqdm(range(10)) : 
+        for idx in range(total_image_number[class_idx]):
+            image = __load_attack_image(class_idx, idx)
+            __do_attack_detect(class_idx, image)
+        
+        for idx in range(total_image_number[class_idx]):
+            image = __load_original_image(class_idx, idx)
+            __do_original_detect(class_idx, image)
+        
     __print_result()
